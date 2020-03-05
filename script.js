e58423984743fe9ca80ae485ed76b74c791c64bd
Rob Sil <djtim2014@gmail.com>
2020-03-05 23:35:58 +0300

    dont finished, day 3330

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const hit = document.getElementById('hit');
 const dead = document.getElementById('dead');
 const enemy = document.getElementById('enemy');
 const again = document.getElementById('again');
+const header = document.querySelector('.header');
 
 const game = {
     ships: [
@@ -24,6 +25,7 @@ const game = {
             hit: ['']
         }
     ],
+    shipCount: 4
 };
 
 const play = {
@@ -45,14 +47,14 @@ const play = {
 
 
 const show = {
-    hit() {
-
+    hit(elem) {
+        this.changeClass(elem, 'hit');
     },
     miss(elem) {
         this.changeClass(elem, 'miss');
     },
-    dead() {
-
+    dead(elem) {
+        this.changeClass(elem, 'dead');
     },
     changeClass(elem, value) {
         elem.className = value;
@@ -62,11 +64,43 @@ const show = {
 const fire = (event) => {
     const target = event.target;
     
-    if (target.tagName === 'TD' && target.classList.value !== 'miss') {
+    if (target.tagName === 'TD' && target.classList.length == 0) {
         play.updateData = 'shot';
         show.miss(target);
 
-        
+        for (let i = 0; i < game.ships.length; i++) {
+            const ship = game.ships[i];
+            const index = ship.location.indexOf(target.id);
+
+            if (index >= 0) {
+                show.hit(target);
+                play.updateData = 'hit';
+                ship.hit[index] = 'x';
+                const life = ship.hit.indexOf('');
+
+                if (life < 0) {
+                    play.updateData = 'dead';
+
+                    for (const id of ship.location) {
+                        show.dead(document.getElementById(id));
+                    }
+
+                    game.shipCount -= 1;
+
+                    if (game.shipCount < 1) {
+                        header.textContent = 'Игра окончена!';
+                        header.style.color = 'red';
+
+                        if (play.shot < play.record || play.record === 0) {
+                            localStorage.setItem('seaBattleRecord', play.shot);
+                            play.updateData = 'record';
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
     
 };
